1c5b905f696cfca01fb952f038cea8c1bd69e267
freqnik <freqnik@protonmail.com>
2024-03-25 02:16:39 -0400

    UpdateNodeScriptions()...Add plan node subscriptions to database with expiration for automatic renewal processing.

---
+++ helpers/update-node-scriptions.py
@@ -0,0 +1,113 @@
+#!/bin/env python3
+
+import scrtxxs
+from urllib.parse import urlparse
+from datetime import datetime
+from sentinel_sdk.sdk import SDKInstance
+from sentinel_sdk.types import PageRequest
+import json
+import argparse
+import pymysql
+import sys
+
+UUID = ""
+
+class UpdateNodeScriptions():
+    
+    def __init__(self):
+        #private_key = self.keyring.get_password("meile-plan", self.wallet_name)
+        
+        #grpcaddr, grpcport = urlparse(scrtxxs.GRPC).netloc.split(":")
+        grpcaddr, grpcport = urlparse(scrtxxs.GRPC).netloc.split(":")
+        
+        self._sdk = SDKInstance(grpcaddr, int(grpcport))
+        self._db = pymysql.connect(host=scrtxxs.MySQLHost,
+                         port=scrtxxs.MySQLPort,
+                         user=scrtxxs.MySQLUsername,
+                         passwd=scrtxxs.MySQLPassword,
+                         db=scrtxxs.MySQLDB,
+                         charset='utf8mb4',
+                         cursorclass=pymysql.cursors.DictCursor)
+        
+    
+    def qs_on_plan(self, nodes):
+        subscriptions = self._sdk.subscriptions.QuerySubscriptionsForAccount(address=scrtxxs.WalletAddress, pagination=PageRequest(limit=1000))
+        
+        SubsNodesInfo = [{
+            'Denom': "",  # TODO: (?)
+            'Deposit': f"{subscription.deposit.amount}{subscription.deposit.denom}",
+            'Gigabytes': f"{subscription.gigabytes}",
+            'Hours': f"{subscription.hours}",
+            'ID': f"{subscription.base.id}",
+            'Inactive Date': datetime.fromtimestamp(subscription.base.inactive_at.seconds).strftime('%Y-%m-%d %H:%M:%S.%f'), # '2024-03-26 19:37:52.52297981',
+            'Node': subscription.node_address,
+            'Owner': subscription.base.address,
+            'Plan': '0',  # TODO: (?)
+            'Status': ["unspecified", "active", "inactive_pending", "inactive"][subscription.base.status],
+        } for subscription in subscriptions]
+        SubsNodesInfo.pop(0)
+        
+        node_subs_on_plan = []
+        
+        for s in SubsNodesInfo:
+            for n in nodes:
+                if n == s['Node']:
+                    node_subs_on_plan.append(s)
+                    
+                    
+        return node_subs_on_plan
+        
+    def GetNodesOnPlan(self, uuid):    
+        query = f"SELECT node_address FROM plan_nodes WHERE uuid = '{uuid}';"
+        
+        c = self._db.cursor()
+        c.execute(query)
+        
+        nodes = []
+        
+        for node in c.fetchall():
+            nodes.append(node['node_address'])
+            
+        return nodes
+
+    def UpdateSubsExpiration(self, subs, uuid):
+        
+        c = self._db.cursor()
+        
+        query = f"SELECT * FROM meile_plans WHERE uuid = '{uuid}';"
+        
+        c.execute(query)
+        plan_data = c.fetchone()
+        
+        for s in subs:
+            q = '''
+            REPLACE INTO plan_node_subscriptions (node_address, uuid, plan_id, plan_subscription_id, node_subscription_id, deposit, hours, inactive_date)
+            VALUES ("%s", "%s", %d, %d, %d, "%s", %d, "%s");
+            ''' % (s['Node'],
+                   uuid,
+                   int(plan_data['plan_id']),
+                   int(plan_data['subscription_id']),
+                   int(s['ID']),
+                   s['Deposit'],
+                   int(s['Hours']),
+                   str(s['Inactive Date']))
+            
+            c.execute(q)
+            self._db.commit()
+        
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Meile Plan Subscriber - v0.1 - freQniK")
+    parser.add_argument('--uuid', help="--uuid <uuid> , plan uuid", metavar="uuid")
+    
+    args = parser.parse_args()
+    
+    if not args.uuid:
+        parser.print_help()
+        sys.exit(1)
+        
+    uns = UpdateNodeScriptions()
+    nodes = uns.GetNodesOnPlan(args.uuid)
+    subs_on_plan = uns.qs_on_plan(nodes)
+    
+    uns.UpdateSubsExpiration(subs_on_plan, args.uuid)
